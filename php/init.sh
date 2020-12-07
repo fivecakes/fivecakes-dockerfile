ffd6e3a1adda716b21276059dc5a7621ab6dfc5f
kuppo <kupposhadow@gmail.com>
2020-12-07 19:43:35 +0800

    add by kuppo

--- php/init.sh
+++ php/init.sh
@@ -14,6 +14,9 @@ mkdir -p /home/git/.ssh/
 cp /workspace/ssh/authorized_keys /home/git/.ssh/authorized_keys
 chown -R git:www-data /home/git/.ssh/
 
+git config --global user.email "git@fivecakes.com"
+git config --global user.name "hooks"
+
 # 临时解决letsencrypt被墙问题
 echo "23.32.3.72     ocsp.int-x3.letsencrypt.org" >> /etc/hosts
 
