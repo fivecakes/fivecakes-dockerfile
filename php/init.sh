791f1d29497a3f4e41c37aa8aad1d2d262b29c9a
kuppo <kupposhadow@gmail.com>
2020-12-07 16:15:40 +0800

    add by kuppo

--- php/init.sh
+++ php/init.sh
@@ -6,18 +6,13 @@ touch /workspace/log/php-fpm/access.log /workspace/log/php-fpm/error.log
 mkdir -p /workspace/ssh/
 
 if [ ! -f "/workspace/ssh/authorized_keys" ]; then
-	ssh-keygen -t rsa -N "" -f /workspace/ssh/git@skynet
-	cat /workspace/ssh/git@skynet.pub >> /workspace/ssh/authorized_keys
+	ssh-keygen -t rsa -N "" -f /workspace/ssh/key4skynet
+	cat /workspace/ssh/key4skynet.pub >> /workspace/ssh/authorized_keys
 fi
 
-mkdir -p /home/git/.ssh/
-cp /workspace/ssh/authorized_keys /home/git/.ssh/authorized_keys
-chown -R git:git /home/git/.ssh/
-
-# 查找并替换git所在行/bin/bash为/usr/bin/git-shell
-# line=`sed -n  '/git:/=' /etc/passwd`
-# sed -i "${line}d" /etc/passwd
-# echo "git:x:1001:1000:,,,:/home/git:/usr/bin/git-shell" >> /etc/passwd
+mkdir -p /home/www-data/.ssh/
+cp /workspace/ssh/authorized_keys /home/www-data/.ssh/authorized_keys
+chown -R www-data:www-data /home/www-data/.ssh/
 
 # 临时解决letsencrypt被墙问题
 echo "23.32.3.72     ocsp.int-x3.letsencrypt.org" >> /etc/hosts
