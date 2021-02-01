1a68db72d7336b209273822fba20abecde8bfd2d
kuppo <kupposhadow@gmail.com>
2021-02-01 14:55:27 +0800

    add by kuppo

--- nodejs/init.sh
+++ nodejs/init.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# mathjax version 3
-cd /deploy/mathjax3/
-node -r esm index.js --port 6000 &
-
 # mathjax version 2
 node /deploy/math/index.js --port 6001 &
 node /deploy/math/index.js --port 6002 &
